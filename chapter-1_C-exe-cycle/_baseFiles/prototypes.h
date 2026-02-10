e2b374bad0304697fc6e6de3139a2a025dcb2d01
Serajus Salehin <serajussalehin21@gmail.com>
2026-02-10 15:43:54 +0600

    Delete chapter-1_C-exe-cycle/_baseFiles directory

--- chapter-1_C-exe-cycle/_baseFiles/prototypes.h
+++
@@ -1,7 +0,0 @@
-#define pi 3.1416
-
-/* Calculates area of a triangle using base and height */
-float areaOfTriangle(float base, float height);
-
-/* Calculates area of a circle using radius */
-float areaOfCircle(float radius);
