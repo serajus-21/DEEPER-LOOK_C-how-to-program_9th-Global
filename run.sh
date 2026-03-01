1dd40b704c5c5abbc0bbe95fd5c9518cedbcca5b
serajus-21 <serajussalehin21@gmail.com>
2026-03-01 11:11:06 +0600

    script

---
+++ run.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+for (( chapter=6; chapter<=15; chapter++ ))
+do
+    mkdir chapter-$chapter
+done
+Community
+For open source projects
+
+$0
+/mo
+
+GET FREE LICENSE
+Highlights:
+
+Unlimited simulations
+
+Unlimited public projects
+
+Virtual WiFi
+
+Hobby
+For personal projects
+
+$7
+$5.6
+/mo
+
+￼
+Annual billing
+￼CHOOSE HOBBY
+Highlights:
+
+100 monthly fast build minutes
+
+Unlisted projects
+
+Upload custom libraries
+
+Private IoT Gateway
+
+Hobby+
+For advanced users
+
+$12
+$8.1
+/mo
+
+￼
+Annual billing
+￼CHOOSE HOBBY+
+Highlights:
+
+500 monthly fast build minutes
+
+Wokwi for VS Code
+
+Unlisted projects
+
+Upload custom libraries
+
+Private IoT Gateway
+
+Wokwi Pro
+For commercial projects
+
+$25
+$20
+/seat/mo
+
+￼
+Annual billing
+￼CHOOSE PRO
+Highlights:
+
+1000 monthly fast build minutes
+
+2000 monthly CI minutes
+
+VS Code offline plug-in
+
+Team billing and invoices
+
+Unlisted projects
+
+Upload custom libraries
+
+Private IoT Gateway
+
+Community
+For open source projects
+
+$0
+/mo
+
+GET FREE LICENSE
+Highlights:
+
+Unlimited simulations
+
+Unlimited public projects
+
+Virtual WiFi
+
+Hobby
+For personal projects
+
+$7
+$5.6
+/mo
+
+￼
+Annual billing
+￼CHOOSE HOBBY
+Highlights:
+
+100 monthly fast build minutes
+
+Unlisted projects
+
+Upload custom libraries
+
+Private IoT Gateway
+
+Hobby+
+For advanced users
+
+$12
+$8.1
+/mo
+
+￼
+Annual billing
+￼CHOOSE HOBBY+
+Highlights:
+
+500 monthly fast build minutes
+
+Wokwi for VS Code
+
+Unlisted projects
+
+Upload custom libraries
+
+Private IoT Gateway
+
+Wokwi Pro
+For commercial projects
+
+$25
+$20
+/seat/mo
+
+￼
+Annual billing
+￼CHOOSE PRO
+Highlights:
+
+1000 monthly fast build minutes
+
+2000 monthly CI minutes
+
+VS Code offline plug-in
+
+Team billing and invoices
+
+Unlisted projects
+
+Upload custom libraries
+
+Private IoT Gateway
+
